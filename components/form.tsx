d0e20cc72fcdff0bd36cdeea82bcb27cbdfafee5
LDNCWRB <londoncodeworkswebdevprogramrb@outlook.com>
2021-05-19 11:22:18 +0100

    removes form module css

--- components/form.tsx
+++ components/form.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import Heading1 from '../elements/Heading1';
 import SubmitButton from '../elements/SubmitButton';
 import TextInput from '../elements/TextInput';
-import styles from './form.module.css';
 import { postTestimonial } from '../services/airTableService';
 import testimonial from '../interfaces/testimonial';
 
@@ -60,69 +59,59 @@ const Form: FC = () => {
         that compromise the anonymity of the testimony, your testimony will not
         be posted.
       </p>
-      <div className={styles.event__creator}>
-        <div className={styles.input_testimony}>
-          <h3>Testimony</h3>
-          <br />
-          <TextInput
-            color="primary"
-            bg="secondary"
-            name="testimony"
-            onChange={e => handleChange(e)}
-            value={testimonial.testimony}
-            placeholder="Insert a testimonial..."
-            required
-          />
-        </div>
-        <br />
-        <div className={styles.input_location}>
-          <h3>
-            School / University / Organisation (Please include if possible)
-          </h3>
-          <br />
-          <TextInput
-            color="primary"
-            bg="secondary"
-            name="location"
-            onChange={e => handleChange(e)}
-            value={testimonial.location}
-            placeholder="Insert a location..."
-            required
-          />
-        </div>
-        <br />
-        <div className={styles.input_consent}>
-          <input
-            name="consent"
-            onChange={e => handleChange(e)}
-            checked={testimonial.consent}
-            type="checkbox"
-            required
-          ></input>
-          <h3> I have read and agree with the consent details</h3>
-        </div>
-        <br />
-        <p>
-          *Organisation covers any business, government, religious or military
-          workplace/institution
-        </p>
-        <p>
-          *By posting your testimony & ticking you agree to our{' '}
-          <a href="">Privacy Policy</a>
-        </p>
-        <p>
-          - You also agree to the conditions of anonymity of yourself and all
-          persons mentioned - this pertains also to not copying testimonies on
-          the site with the intention of reposting elsewhere where previously
-          anonymous details may be revealed.
-        </p>
-        <p>
-          - Please submit responsibly: this is an anonymous outlet only and
-          Everyone's Invited does not condone the naming of any involved.
-          Everyone’s Invited is not a substitute for a legitimate form of
-          reporting, through an institution or via the police.
-        </p>
-      </div>
+      <h3>Testimony</h3>
+      <br />
+      <TextInput
+        color="primary"
+        bg="secondary"
+        name="testimony"
+        onChange={e => handleChange(e)}
+        value={testimonial.testimony}
+        placeholder="Insert a testimonial..."
+        required
+      />
+      <br />
+      <h3>School / University / Organisation (Please include if possible)</h3>
+      <br />
+      <TextInput
+        color="primary"
+        bg="secondary"
+        name="location"
+        onChange={e => handleChange(e)}
+        value={testimonial.location}
+        placeholder="Insert a location..."
+        required
+      />
+      <br />
+      <input
+        name="consent"
+        onChange={e => handleChange(e)}
+        checked={testimonial.consent}
+        type="checkbox"
+        required
+      ></input>
+      <h3> I have read and agree with the consent details</h3>
+      <br />
+      <p>
+        *Organisation covers any business, government, religious or military
+        workplace/institution
+      </p>
+      <p>
+        *By posting your testimony & ticking you agree to our{' '}
+        <a href="">Privacy Policy</a>
+      </p>
+      <p>
+        - You also agree to the conditions of anonymity of yourself and all
+        persons mentioned - this pertains also to not copying testimonies on the
+        site with the intention of reposting elsewhere where previously
+        anonymous details may be revealed.
+      </p>
+      <p>
+        - Please submit responsibly: this is an anonymous outlet only and
+        Everyone's Invited does not condone the naming of any involved.
+        Everyone’s Invited is not a substitute for a legitimate form of
+        reporting, through an institution or via the police.
+      </p>
       <br />
       <SubmitButton label="Submit" />
     </FormContainer>

4917c1318322db0ec5c5947466527ccb10a66cb8
LDNCWRB <londoncodeworkswebdevprogramrb@outlook.com>
2021-05-19 16:08:54 +0100

    removes css files + creates styled components for whole form #5

--- components/elements/Container.tsx
+++ components/elements/Container.tsx
@@ -1,13 +1,18 @@
 import { FC } from 'react';
 import BaseContainer from './BaseContainer';
 
-const Container: FC = ({ children }) => {
+interface Props {
+  // label: string;
+}
+
+const Container: FC<Props> = ({ children }) => {
   return (
     <BaseContainer
       color="primary"
       m={4}
       py={4}
       px={4}
+      // height="50px"
     >
       {children}
     </BaseContainer>
